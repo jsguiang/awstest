2f5a4666e37c35223529e51d2f2bcd35e72c5b57
jsguiang <jllsabado@gmail.com>
2020-03-25 13:19:49 +0800

    Updates

---
+++ precache-manifest.ea5b5821f2b3460e6ca7e52395943d72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d80758aea96a3311dd7587c68ef9ff",
+    "url": "/awstest/index.html"
+  },
+  {
+    "revision": "5eb8aad8d076913501ce",
+    "url": "/awstest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a244d03c9afdb1f5f258",
+    "url": "/awstest/static/js/2.e0212a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awstest/static/js/2.e0212a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8aad8d076913501ce",
+    "url": "/awstest/static/js/main.93710d5d.chunk.js"
+  },
+  {
+    "revision": "1bb0c40b6c5ff7016890",
+    "url": "/awstest/static/js/runtime-main.8dafa069.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awstest/static/media/logo.5d5d9eef.svg"
+  }
+]);
